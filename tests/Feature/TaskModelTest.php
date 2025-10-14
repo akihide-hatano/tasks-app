054d37a8e5a9a91cded4fff8248d2f77f3577e50
akihide-hatano <a.h.19841202@gmail.com>
2025-10-14 20:13:33 +0900

    TaskModelTest.phpを修正

--- tests/Feature/TaskModelTest.php
+++ tests/Feature/TaskModelTest.php
@@ -16,12 +16,23 @@ class TaskModelTest extends TestCase
         $user = User::factory()->create();
 
          // 全角スペースや前後空白を含む入力
-         $task = Task::create([
-                     'user_id' => $user->id,
+        $task = Task::create([
+            'user_id' => $user->id,
             'title'   => '  テ. ス. ト  ',   // 前後空白＋スペース多め
             'is_done' => '0',                // 文字列でもOK
         ]);
+        $this->assertSame('テ. ス. ト', $task->title);
+    }
 
-         $this->assertSame('テ. ス. ト', $task->title);
+    public function is_done_boole():void{
+        $task = Task::factory()->create(['is_done'=>'1']);
+
+        $this->assertIsBool($task->is_done);
+        $this->assertTrue($task->is_done);
+
+        $task->is_done = 0;
+        $task->save();
+        $task->refresh();
+        $this->assertFalse($task->is_done);
     }
 }
