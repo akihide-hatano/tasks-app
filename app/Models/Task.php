b0a18b528f473e57317d3c18c19dc79d45880413
akihide-hatano <a.h.19841202@gmail.com>
2025-10-14 13:12:35 +0900

    Models/Task.phpを修正。

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Task extends Model
 {
     protected $fillable = ['title','is_done'];
     protected $casts = ['is_done'=>'boolean'];
 
+
+    public function title():Attribute{
+        return Attribute::make(
+             set: fn ($v) => mb_convert_kana(trim((string)$v), 's') // 前後空白除去＋スペース正規化
+        );
+    }
     public function user():BelongsTo{
         return $this->belongsTo(User::class);
     }
