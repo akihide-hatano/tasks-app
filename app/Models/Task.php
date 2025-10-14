7ab37d96c2d426e9c089284b884693562cae0cb2
akihide-hatano <a.h.19841202@gmail.com>
2025-10-14 12:59:43 +0900

    Task.phpのmodelを修正

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['title','is_done'];
+    protected $casts = ['is_done'=>'boolean'];
+
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
