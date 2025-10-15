7d88af40ce2cce6de211e8ca80203df7767cf86d
akihide-hatano <a.h.19841202@gmail.com>
2025-10-15 16:35:24 +0900

    web.phpとTaskControllerTest.phpを編集しました

---
+++ tests/Feature/TaskControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Task;
+
+
+class TaskControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /* ---------- 認証まわり ---------- */
+
+    public function test_guest_is_redirected_to_login(): void
+    {
+        $this->get('/tasks')->assertRedirect('/login');
+        $this->post('/tasks', [])->assertRedirect('/login');
+    }
+}
