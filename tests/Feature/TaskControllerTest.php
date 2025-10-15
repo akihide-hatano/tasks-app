c3751973c37219df312e0b9d864e8c90869ebc04
akihide-hatano <a.h.19841202@gmail.com>
2025-10-15 16:52:50 +0900

    TaskControllerTestにtest_index_show_onlyのtestを追加

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -20,4 +20,23 @@ class TaskControllerTest extends TestCase
         $this->get('/tasks')->assertRedirect('/login');
         $this->post('/tasks', [])->assertRedirect('/login');
     }
+
+    /*----------- index --------------*/
+    
+    public function test_index_shows_only_my_tasks():void
+    {
+        $me = User::factory()->create();
+        $other = User::factory()->create();
+
+        Task::factory()->for($me)->count(2)->create(['title'=>'mine']);
+        Task::factory()->for($other)->count(1)->create(['title'=>'other']);
+
+        $res = $this->actingAs($me)->get('/tasks');
+
+        $res->assertOk()
+            ->assertViewIs('tasks.index')
+            ->assertSee('mine')
+            ->assertDontSee('others'); // 他人のは出ない
+
+    }
 }
